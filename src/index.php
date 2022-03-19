d89e08707c4898766a152a8991927d13c0f13fcc
Christian Peralta <christianipc.1987@gmail.com>
2022-03-19 15:52:36 -0300

    Hago una prueba de la conexion a la base de datos y obtencion de datos con la estructura armada

--- src/index.php
+++ src/index.php
@@ -1,23 +1,8 @@
 <?php
 
-// echo "entro al script php hola christian";
+require_once('./controller/Autoload.php');
 
-$mysqli = new mysqli("mysql_db_apiphp:3306", "root", "animales#", "animales");
-if($mysqli->connect_errno){
-    die("Falla en la conexión ". mysqli_connect_error());
-}
-
-$consulta = "SELECT * FROM mascota";
-$resultado = $mysqli->query($consulta);
-
-echo "<h2>Mascotas</h2>";
-
-while ($fila = $resultado->fetch_assoc()) {
-    echo "<ul>";
-    echo "<li> id = " . $fila['id'] . "</li>";
-    echo "<li> nombre = " . $fila['nombre'] . "</li>";
-    echo "</ul>";
-}
-
-echo $mysqli->host_info . "\n";
+$autoload = new Autoload();
+$route = isset($_GET['route']) ? $_GET['route'] : 'home';
+$app = new Router($route);
 
