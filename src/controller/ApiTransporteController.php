b574a5d94f9b12cc77b6fa0271abe1480eb85ced
Christian Peralta <christianipc.1987@gmail.com>
2023-04-20 16:11:04 -0300

    Agrego headers para metodos http tambien agrego coleccion para postman

--- src/controller/ApiTransporteController.php
+++ src/controller/ApiTransporteController.php
@@ -1,7 +1,10 @@
 <?php
 
 header('Content-type: application/json; charset=utf8');
-// header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
 class ApiTransporteController{
 	private $route = 'api';
